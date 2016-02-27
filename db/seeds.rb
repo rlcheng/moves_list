070de24dab8c0b8900098c3a1963065e92a26812
Richard Cheng <rlcheng.7@gmail.com>
2016-02-26 23:36:34 -0800

    create seeds file to have show a complete character moves list for a game

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Game
+game = Game.create!(title: "Samurai Shodown 2")
+#Character
+character = game.characters.create!(name: "Genjuro")
+#Moves
+character.moves.create!(name: "Toha Kouyokujin", input: "FDDF+Slash")
+character.moves.create!(name: "Sanrensatsu", input: "QCF+Slash *can be repeated up to 3x")
+character.moves.create!(name: "Oukazan", input: "QCB+Slash")
+character.moves.create!(name: "Super Deformed", input: "HCBFB+B")
+character.moves.create!(name: "Power Special - Gokouzan", input: "HCBF+A")
