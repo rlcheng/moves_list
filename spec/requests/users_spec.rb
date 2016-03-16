16d94b0dcca0865e8571da1ddf240203a7b6bf15
Richard Cheng <rlcheng.7@gmail.com>
2016-03-16 14:12:17 -0700

    Add email downcase for SignUp and LogIn and update specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,6 +5,8 @@ describe "user", type: :request do
   let!(:new_user) { FactoryGirl.build(:user) }
   let(:new_params) {{ user: { email: new_user.email,
     password: new_user.password, password_confirmation: new_user.password} } }
+  let(:new_params2) {{ user: { email: new_user.email.upcase,
+    password: new_user.password, password_confirmation: new_user.password} } }
 
   it "should render the sign up page" do
     get '/sign_up'
@@ -27,4 +29,12 @@ describe "user", type: :request do
     expect(response.body).to include("Email is invalid")
   end
 
+  it "should not create user if new user's email is capitalized of existing" do
+    post '/users', new_params
+    expect(response.code).to eq("302")
+    post '/users', new_params2
+    expect(response).to render_template('new')
+    expect(response.body).to include("Form is invalid")
+    expect(response.body).to include("Email has already been taken")
+  end
 end
