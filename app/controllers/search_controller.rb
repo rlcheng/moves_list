da8a413689b69aaa3298985eb425c076a3d21d44
Richard Cheng <rlcheng.7@gmail.com>
2016-03-23 13:38:44 -0700

    Update search to include Character and Move

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,12 @@ class SearchController < ApplicationController
   def search
     if params[:q].nil?
       @games = []
+      @characters = []
+      @moves = []
     else
       @games = Game.search params[:q]
+      @characters = Character.search params[:q]
+      @moves = Move.search params[:q]
     end
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,17 @@
 class Character < ActiveRecord::Base
+  include Searchable
   belongs_to :game
   has_many :moves, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2 }
 end
+
+# Delete the previous characters index in Elasticsearch
+Character.__elasticsearch__.client.indices.delete index: Character.index_name rescue nil
+
+# Create the new index with the new mapping
+Character.__elasticsearch__.client.indices.create \
+  index: Character.index_name,
+  body: { settings: Character.settings.to_hash, mappings: Character.mappings.to_hash }
+
+# Index all character records from the DB to Elasticsearch
+Character.import
